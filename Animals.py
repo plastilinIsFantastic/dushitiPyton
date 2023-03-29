fec3e901a8dcbbc1bf59b34ba6a03d13e30c511b
appa <appabisonUpUp@gmail.com>
2023-03-29 21:04:23 +0300

    #

--- Animals.py
+++ Animals.py
@@ -2,29 +2,20 @@ class Animal():
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+        print(f'New born {__class__.__name__}!')
     
     def speak(self):
         print(f'It\'s {self.name}: {self.age} old')
 
 class Cat(Animal):
-    def __init__(self, name: str, age: int):
-        super().__init__(name, age)
-        print('New born Cat!')
-    
-    def getVoice(self):
-        print('Meow')
+    def meow(self):
+        print('Meow Meow Meow')
 
 class Dog(Animal):
-    def __init__(self, name: str, age: int):
-        super().__init__(name, age)
-        print('New born Dog!')
+    def gav(self):
+        print('Woof Woof Woof')
 
-    def getVoice(self):
-        print('Meow')
-
-cat = Cat("kitty", 2)
-cat.speak()
-cat.getVoice()
+Cat("kitty", 2).meow()
 
 dog = Dog("puppy", 1)
 dog.speak()
