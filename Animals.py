ff91b336d2a41cb27b34bb20500a178b559809e1
appa <appabisonUpUp@gmail.com>
2023-03-29 18:53:11 +0300

    add class Cat and Dog

--- Animals.py
+++ Animals.py
@@ -6,5 +6,16 @@ class Animal():
     def speak(self):
         print(f'It\'s {self.name}: {self.age} old')
 
-cat = Animal("cat", 10)
+class Cat(Animal):
+    def speak(self):
+        print(f'Kitten says Meow! It\'s name: {self.name}, old: {self.age}')
+
+class Dog(Animal):
+    def speak(self):
+        print(f'Doogo says Woof! It\'s name: {self.name}, old: {self.age}')
+
+cat = Cat("kitty", 2)
 cat.speak()
+
+dog = Dog("puppy", 1)
+dog.speak()
